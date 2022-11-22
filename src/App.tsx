547f5adf0160134f1f69297ed9ad4990f6244f2a
Romel Kenneth Recabo <1rkennethr1@gmail.com>
2022-11-22 22:45:25 +0800

    Patuti sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import ColorSequence from './components/ColorSequence';
 import TicTacToe from './components/TicTacToe';
+import Patuti from './components/Patuti';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <TicTacToe></TicTacToe>
+      {/* <TicTacToe></TicTacToe> */}
+      <Patuti/>
       </header>
     </div>
   );
